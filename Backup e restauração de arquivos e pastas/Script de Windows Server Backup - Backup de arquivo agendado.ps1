52b285490b303edda64d5cfd164e1b9bf46f8551
Gabriel Luiz <gabrielluiz@msn.com>
2023-02-14 22:27:47 -0300

    Script de Windows Server Backup - Backup de arquivo agendado
    Script de Windows Server Backup - Backup de arquivo agendado

---
+++ Backup e restauração de arquivos e pastas/Script de Windows Server Backup - Backup de arquivo agendado.ps1
@@ -0,0 +1,39 @@
+﻿## Script de Windows Server Backup - Backup de arquivo agendado - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+$Policy = New-WBPolicy # Crie um objeto de política vazio. Variável $Policy.
+
+$Filespec = New-WBFileSpec -FileSpec C:\Exemplo\1.bmp # Especifica o arquivo que será realizado o backup.
+
+$volumeBackupLocation = New-WBBackupTarget -VolumePath D: # Especifica o local aonde será armazenado o backup.
+
+Add-WBFileSpec -Policy $Policy -FileSpec $FileSpec # Especifica qual será o objeto do backup. Variável $Filespec.
+
+Add-WBBackupTarget -Policy $Policy -Target $volumeBackupLocation # Especifica qual será o local do backup. Variável $volumeBackupLocation.
+
+Set-WBSchedule -Policy $Policy 09:00, 19:00 # Especifica qual será o horário do backup, neste exemplo será às 09:00 e 19:00 horas.
+
+Set-WBPolicy -Policy $Policy # Salva a política de backup criada.
+
+
+<# 
+
+Referências:
+
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/new-wbpolicy?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/new-wbfilespec?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/new-wbbackuptarget?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/add-wbfilespec?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/add-wbbackuptarget?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/set-wbschedule?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/set-wbpolicy?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+
+#>
