44982ec4dd532d55008997a483429fcf2122fc54
Gabriel Luiz <gabrielluiz@msn.com>
2023-02-14 22:32:46 -0300

    Script de Windows Server Backup - Restauração de arquivos
    Script de Windows Server Backup - Restauração de arquivos

---
+++ Backup e restauração de arquivos e pastas/Script de Windows Server Backup - Restauração de arquivos.ps1
@@ -0,0 +1,62 @@
+﻿## Script de  Windows Server Backup - Restauração de Arquivos - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+Get-WBBackupSet # Verifica as versões de backup disponíveis.
+
+$myBackup = Get-WBBackupSet | Where-Object VersionId -eq 08/07/2022-15:25 # Seleciona a versão do backup a ser restaurada.
+
+Start-WBFileRecovery -BackupSet $myBackup -SourcePath C:\Exemplo\1.bmp # Inicia o processo de restauração do arquivo.
+
+
+
+<# 
+
+Explicação do comando: 
+
+O parâmetro –Recursive–TargetPath
+
+No caso de uma restauração redirecionada ou uma restauração para um local diferente, o alvo será o destino dos arquivos recuperados.
+
+
+O parâmetro -Option
+
+
+Para definir o tipo de restauração, adicione o comando com uma das seguintes configurações:
+
+
+-Option SkipIfExists
+
+Esta configuração só restaurará arquivos que foram movidos ou excluídos.
+
+
+-Option CreateCopyIfExists
+
+Esta é uma configuração padrão. Se o arquivo existir, essa configuração renomeará o arquivo restaurado.
+
+
+-Option OverwriteIfExists
+
+
+Se o arquivo existir, esta configuração irá substituí-lo com o arquivo restaurado.
+
+
+
+Exemplo do uso do parâmetro -Option : Start-WBFileRecovery -BackupSet $myBackup -SourcePath C:\Exemplo\1.bmp -Recursive -TargetPath C:\ -Option CreateCopyIfExists
+
+
+
+#>
+
+
+<# 
+
+Referências:
+
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/get-wbbackupset?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/start-wbfilerecovery?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+
+#>
+
