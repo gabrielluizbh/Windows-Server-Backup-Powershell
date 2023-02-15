8166b0bf07cfafc5af9e8472d26cf9fc972b79ff
Gabriel Luiz <gabrielluiz@msn.com>
2023-02-14 22:58:13 -0300

    Script de Windows Server Backup - Restauração do Dados do aplicativo
    Script de Windows Server Backup - Restauração do Dados do aplicativo

---
+++ Backup e restauração de Dados do aplicativo/Script de Windows Server Backup - Restauração do Dados do aplicativo.ps1
@@ -0,0 +1,23 @@
+﻿## Script de Windows Server Backup - Restauração do Estado do Sistema - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+Get-WBBackupSet # Verifica as versões de backup disponíveis.
+
+$myBackup = Get-WBBackupSet | Where-Object VersionId -eq 10/17/2022-23:08 # Seleciona a versão do backup a ser restaurada.
+
+$myApp = $myBackup.Application | Where-Object Identifier -eq Registry # Seleciona o aplicativo a ser restaurado.
+
+Start-WBSystemStateRecovery -BackupSet $myBackup # Inicia o processo de restauração dos Dados do aplicativo.
+
+
+<# 
+
+Referências:
+
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/Get-WBBackupset?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/windowsserverbackup/start-wbbackup?view=windowsserver2022-ps&WT.mc_id=WDIT-MVP-5003815
+
+
+#>
